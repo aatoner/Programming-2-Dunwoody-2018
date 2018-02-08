b6aab9faa5ae68de349e29f6b2ea3b3c06c8fbfe
aatoner <35305696+aatoner@users.noreply.github.com>
2018-02-08 14:03:54 -0600

    Add files via upload

---
+++ lab1assignment/etsystatsjson.js
@@ -0,0 +1,279 @@
+var etsyStats =[
+
+  {
+    "name": "Aislinn Martin",
+    "numberofitems": 1,
+    "totalsale": 40,
+    "month": "January",
+    "year": 2018,
+    "city": "Victory",
+    "state": "null",
+    "country": "Australia"
+  },
+  {
+    "name": "Kelsey McKim",
+    "numberofitems": 1,
+    "totalsale": 20,
+    "month": "January",
+    "year": 2018,
+    "city": "Indianapolis",
+    "state": "Indiana",
+    "country": "USA"
+  },
+  {
+    "name": "Laura Lampley",
+    "numberofitems": 1,
+    "totalsale": 25,
+    "month": "January",
+    "year": 2018,
+    "city": "Los Angeles",
+    "state": "California",
+    "country": "USA"
+  },
+  {
+    "name": "Sonia Clark",
+    "numberofitems": 1,
+    "totalsale": 20,
+    "month": "January",
+    "year": 2018,
+    "city": "Oakland",
+    "state": "California",
+    "country": "USA"
+  },
+  {
+    "name": "Shannon Vento",
+    "numberofitems": 2,
+    "totalsale": 50,
+    "month": "December",
+    "year": 2017,
+    "city": "Golden Valley",
+    "state": "Minnesota",
+    "country": "USA"
+  },
+  {
+    "name": "Vonda Nutt",
+    "numberofitems": 1,
+    "totalsale": 25,
+    "month": "December",
+    "year": 2017,
+    "city": "Irving",
+    "state": "Texas",
+    "country": "USA"
+  },
+  {
+    "name": "Jordan Beyer",
+    "numberofitems": 1,
+    "totalsale": 25,
+    "month": "December",
+    "year": 2017,
+    "city": "Palm Harbor",
+    "state": "Florida",
+    "country": "USA"
+  },
+  {
+    "name": "Samantha Weick",
+    "numberofitems": 2,
+    "totalsale": 45,
+    "month": "December",
+    "year": 2017,
+    "city": "Minneapolis",
+    "state": "Minnesota",
+    "country": "USA"
+  },
+  {
+    "name": "Julie Hanna",
+    "numberofitems": 1,
+    "totalsale": 15,
+    "month": "December",
+    "year": 2017,
+    "city": "Saint Paul",
+    "state": "Minnesota",
+    "country": "USA"
+  },
+  {
+    "name": "Zambelli Navarro",
+    "numberofitems": 1,
+    "totalsale": 25,
+    "month": "December",
+    "year": 2017,
+    "city": "New York",
+    "state": "New York",
+    "country": "USA"
+  },
+  {
+    "name": "Carmen Murphy",
+    "numberofitems": 1,
+    "totalsale": 25,
+    "month": "December",
+    "year": 2017,
+    "city": "Shannock",
+    "state": "Rhode Island",
+    "country": "USA"
+  },
+  {
+    "name": "Bridget Todd",
+    "numberofitems": 1,
+    "totalsale": 20,
+    "month": "December",
+    "year": 2017,
+    "city": "Washington",
+    "state": "DC",
+    "country": "USA"
+  },
+  {
+    "name": "Ashley Span",
+    "numberofitems": 1,
+    "totalsale": 25,
+    "month": "December",
+    "year": 2017,
+    "city": "Philadephia",
+    "state": "Pennsylvania",
+    "country": "USA"
+  },
+  {
+    "name": "Racheal Sullivan",
+    "numberofitems": 3,
+    "totalsale": 70,
+    "month": "December",
+    "year": 2017,
+    "city": "Brooklyn",
+    "state": "New York",
+    "country": "USA"
+  },
+  {
+    "name": "Anna Pendleton",
+    "numberofitems": 1,
+    "totalsale": 15,
+    "month": "December",
+    "year": 2017,
+    "city": "Minneapolis",
+    "state": "Minnesota",
+    "country": "USA"
+  },
+  {
+    "name": "JoEllen Krupp",
+    "numberofitems": 1,
+    "totalsale": 25,
+    "month": "December",
+    "year": 2017,
+    "city": "Carmel",
+    "state": "Indiana",
+    "country": "USA"
+  },
+  {
+    "name": "Jenny Slatterly",
+    "numberofitems": 1,
+    "totalsale": 20,
+    "month": "November",
+    "year": 2017,
+    "city": "Valencia",
+    "state": "California",
+    "country": "USA"
+  },
+  {
+    "name": "Nicole Wong",
+    "numberofitems": 1,
+    "totalsale": 20,
+    "month": "November",
+    "year": 2017,
+    "city": "San Francisco",
+    "state": "California",
+    "country": "USA"
+  },
+  {
+    "name": "Miko Nanto",
+    "numberofitems": 2,
+    "totalsale": 35,
+    "month": "October",
+    "year": 2017,
+    "city": "Lacey",
+    "state": "Washington",
+    "country": "USA"
+  },
+  {
+    "name": "Stephanie Voss",
+    "numberofitems": 1,
+    "totalsale": 25,
+    "month": "October",
+    "year": 2017,
+    "city": "Minneapolis",
+    "state": "Minnesota",
+    "country": "USA"
+  },
+  {
+    "name": "Kali Clark",
+    "numberofitems": 2,
+    "totalsale": 50,
+    "month": "October",
+    "year": 2017,
+    "city": "Olympia",
+    "state": "Washington",
+    "country": "USA"
+  },
+  {
+    "name": "Gutherie de Bruyn",
+    "numberofitems": 1,
+    "totalsale": 25,
+    "month": "October",
+    "year": 2017,
+    "city": "Pahoa",
+    "state": "Hawaii",
+    "country": "USA"
+  },
+  {
+    "name": "Michelle Brusegaard",
+    "numberofitems": 1,
+    "totalsale": 40,
+    "month": "October",
+    "year": 2017,
+    "city": "Minneapolis",
+    "state": "Minnesota",
+    "country": "USA"
+  },
+  {
+    "name": "Cailin Corbett",
+    "numberofitems": 1,
+    "totalsale": 20,
+    "month": "October",
+    "year": 2017,
+    "city": "Los Angeles",
+    "state": "California",
+    "country": "USA"
+  },
+  {
+    "name": "Amy Ihlan",
+    "numberofitems": 1,
+    "totalsale": 40,
+    "month": "September",
+    "year": 2017,
+    "city": "Saint Paul",
+    "state": "Minnesota",
+    "country": "USA"
+  }
+]
+
+var octoberSales = 0, novemberSales = 0, decemberSales = 0, januarySales = 0, septemberSales = 0;
+for(var i = 0; i < etsyStats.length; i++){
+
+  
+  if(etsyStats[i].month == "October"){
+    octoberSales +=1;
+  }
+  if(etsyStats[i].month == "November"){
+    novemberSales +=1;
+  }
+  if(etsyStats[i].month == "December"){
+    decemberSales +=1;
+  }
+  if(etsyStats[i].month == "January"){
+    januarySales +=1;
+  }
+  if(etsyStats[i].month == "September"){
+    septemberSales +=1;
+  }
+}
+console.log("There are " + octoberSales + " sales for the month of October");
+console.log("There are " + novemberSales + " sales for the month of November.");
+console.log("There are " + decemberSales + " sales for the month of December.");
+console.log("There are " + januarySales + " sales for the month of January");
+console.log("There are " + septemberSales + " sales for the month of September");
